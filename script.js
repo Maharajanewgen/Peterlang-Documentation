3e1330242cb0de7cc471bf27d2eb25864708d304
Maharajanewgen <160841580+Maharajanewgen@users.noreply.github.com>
2024-02-22 14:55:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showContent(contentId) {
+    const contents = document.querySelectorAll('.content');
+    
+    contents.forEach(content => {
+        content.classList.remove('active');
+    });
+
+    const selectedContent = document.getElementById(contentId);
+    if (selectedContent) {
+        selectedContent.classList.add('active');
+    } else {
+        console.error("Content with ID " + contentId + " not found.");
+    }
+}
