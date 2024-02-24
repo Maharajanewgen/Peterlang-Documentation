e759b8fa9978dd3211929a76ff632eacda685666
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-24 10:22:58 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function showContent(contentId) {
-    const contents = document.querySelectorAll('.content');
-    
-    contents.forEach(content => {
-        content.classList.remove('active');
-    });
+    function showContent(contentId) {
+        const contents = document.querySelectorAll('.content');
+        
+        contents.forEach(content => {
+            content.classList.remove('active');
+        });
 
-    const selectedContent = document.getElementById(contentId);
-    if (selectedContent) {
-        selectedContent.classList.add('active');
-    } else {
-        console.error("Content with ID " + contentId + " not found.");
+        const selectedContent = document.getElementById(contentId);
+        if (selectedContent) {
+            selectedContent.classList.add('active');
+        } else {
+            console.error("Content with ID " + contentId + " not found.");
+        }
     }
-}
- 
+
