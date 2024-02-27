d0f509f3037377e0996436e883ce7487e1003123
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-27 18:23:55 +0530

    c

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-   function showContent(contentId) {
-        const contents = document.querySelectorAll('.content');
-        
-        contents.forEach(content => {
-            content.classList.remove('active');
-        });
+function showContent(contentId) {
+    const contents = document.querySelectorAll('.content');
 
-        const selectedContent = document.getElementById(contentId);
-        if (selectedContent) {
-            selectedContent.classList.add('active');
-            // Save the selected content ID to local storage
-        localStorage.setItem('selectedContent', contentId);
-        } else {
-            console.error("Content with ID " + contentId + " not found.");
-        }
-    }
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        var selectedContentId = localStorage.getItem('selectedContent');
-        if (selectedContentId) {
-            showContent(selectedContentId);
-        }
+    contents.forEach(content => {
+        content.classList.remove('active');
     });
+
+    const selectedContent = document.getElementById(contentId);
+    if (selectedContent) {
+        selectedContent.classList.add('active');
+        // Save the selected content ID to local storage
+        localStorage.setItem('selectedContent', contentId);
+    } else {
+        console.error("Content with ID " + contentId + " not found.");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectedContentId = localStorage.getItem('selectedContent');
+    if (!selectedContentId) {
+        console.error("No selected content ID found in local storage.");
+    } else {
+        showContent(selectedContentId);
+    }
+});
