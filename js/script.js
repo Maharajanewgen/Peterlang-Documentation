05dcde20f195b3155ac3591ba3cde9967cf247ba
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-29 10:50:36 +0530

    c

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-// Toggle sidebar visibility when clicking on the main content area
-document.querySelector('.content').addEventListener('click', function() {
-    document.querySelector('.sidebar').classList.toggle('active');
-    document.querySelector('.content').classList.toggle('active');
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch and include the sidebar.html content
+    fetch('sidebar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('sidebarContainer').innerHTML = data;
+  
+        // Toggle sidebar visibility when clicking on the main content area
+        document.querySelector('.content').addEventListener('click', function() {
+          document.querySelector('.sidebar').classList.toggle('active');
+          document.querySelector('.content').classList.toggle('active');
+        });
+      });
   });
   
