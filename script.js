03b2d5a4ebf6f612d77e756aa4ac7b52626dd40c
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-29 09:25:44 +0530

    c

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function getSidebar() {
+    return `
+        <div class="sidebar">
+            <div class="nav-item" onclick="navigateToReanalysis()">
+                <span>&#9654;</span> Reanalysis
+            </div>
+            <div class="nav-item" onclick="navigateToContact()">
+                <span>&#9654;</span> Contact
+            </div>
+        </div>
+    `;
+}
+
 function navigateToReanalysis() {
     window.location.href = 'html/reanalysis/reanalysis.html';
 }
