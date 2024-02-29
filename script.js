ca9181717bbfad8d35ff43563b55122b3b3c059c
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-29 08:49:27 +0530

    c

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-// script.js
-
-function showContent(contentId, className) {
-    var content = document.getElementById(contentId);
-
-    if (content) {
-        if (content.style.display === 'none' || content.style.display === '') {
-            // If content is hidden, load it dynamically
-            fetchContent(contentId, className);
-            content.style.display = 'block';
-        } else {
-            content.style.display = 'none';
-        }
-    } else {
-        console.error('Element with id ' + contentId + ' not found.');
-    }
+function navigateToReanalysis() {
+    window.location.href = 'html/reanalysis/reanalysis.html';
 }
 
-function fetchContent(contentId, className) {
-    var container = document.getElementById('content-container');
-    var filePath = 'html/' + contentId + '/' + contentId + '.html';
-
-    // Fetch content using AJAX (Assuming no external libraries for simplicity)
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            container.innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', filePath, true);
-    xhr.send();
+function navigateToContact() {
+    window.location.href = 'html/contact/contact.html';
 }
