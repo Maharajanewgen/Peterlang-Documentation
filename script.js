10c4e95b8692915d816503ae79b7677fa30cc9c7
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-29 14:19:47 +0530

    c

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+var openedTabs = {}; // Keep track of opened tabs
+
 function showContent(contentId) {
-    // Open content in a new tab with target="_blank"
-    window.open(window.location.href + '#' + contentId, '_blank');
+    // Check if the content is already opened
+    if (!openedTabs[contentId]) {
+        // Open content in a new tab with target="_blank"
+        window.open(window.location.href + '#' + contentId, '_blank');
+        openedTabs[contentId] = true; // Mark the content as opened
+    }
 }
 
 // Check for content id in URL and show the corresponding content
@@ -12,3 +18,30 @@ document.addEventListener('DOMContentLoaded', function () {
         showContent(contentIdFromUrl);
     }
 });
+function sortTable(col) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("issueTable");
+    switching = true;
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+
+            x = rows[i].getElementsByTagName("td")[col];
+            y = rows[i + 1].getElementsByTagName("td")[col];
+
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
