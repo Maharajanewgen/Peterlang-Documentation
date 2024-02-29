105e1f71a03078627d951987898db63e12a34979
NewgenPL <160841580+newgenpl@users.noreply.github.com>
2024-02-29 11:34:51 +0530

    c

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Fetch and include the sidebar.html content
-    fetch('sidebar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('sidebarContainer').innerHTML = data;
-  
-        // Toggle sidebar visibility when clicking on the main content area
-        document.querySelector('.content').addEventListener('click', function() {
-          document.querySelector('.sidebar').classList.toggle('active');
-          document.querySelector('.content').classList.toggle('active');
-        });
-  
-        // Close sidebar when clicking on a sidebar link
-        const sidebarLinks = document.querySelectorAll('.sidebar a');
-        sidebarLinks.forEach(link => {
-          link.addEventListener('click', function() {
-            document.querySelector('.sidebar').classList.remove('active');
-            document.querySelector('.content').classList.remove('active');
-          });
+  // Fetch and include the sidebar.html content
+  fetch('sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('sidebarContainer').innerHTML = data;
+
+      // Toggle sidebar visibility when clicking on the main content area
+      document.querySelector('.content').addEventListener('click', function() {
+        document.querySelector('.sidebar').classList.toggle('active');
+        document.querySelector('.content').classList.toggle('active');
+      });
+
+      // Close sidebar when clicking on a sidebar link
+      const sidebarLinks = document.querySelectorAll('.sidebar a');
+      sidebarLinks.forEach(link => {
+        link.addEventListener('click', function() {
+          document.querySelector('.sidebar').classList.remove('active');
+          document.querySelector('.content').classList.remove('active');
         });
       });
-  });
-  
+    });
+});
